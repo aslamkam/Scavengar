3705472d12791a2e5cfa9bd121458299abb3ec40
Kamal <crispevening@gmail.com>
2018-09-28 19:04:39 -0400

    Add everything

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Completed;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+    private BoardManager boardScript;
+    private int lvl = 3;
+
+    void awake()
+    {
+        if(instance == null)
+        {
+            instance = null;
+        } else if (instance != this) {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+    }
+
+    void InitGame()
+    {
+        boardScript.SetupScene(lvl);
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
